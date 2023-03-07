e184dc1737f7eb91528495656075ffe72c145f33
Daniel Garcia <danielgarcia@nkbr.email>
2023-03-07 17:35:33 -0600

    skeleton in place Still need to implement render() and write_image()

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,89 @@
+use std::path::PathBuf;
+use num::Complex;
+use clap::{Parser, ValueEnum};
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    #[arg(short = 'F', long)]
+    file: PathBuf,
+
+    #[arg(short = 'W', long)]
+    width: usize,
+
+    #[arg(short = 'H', long)]
+    height: usize,
+
+    #[arg(short = 'L', long)]
+    upper_left: Complex<f64>,
+
+    #[arg(short = 'R', long)]
+    lower_right: Complex<f64>,
+
+    #[arg(short = 'l', long)]
+    limit: Limit,
+}
+#[derive(Debug, Clone, Copy, ValueEnum)]
+enum Limit {
+    Low,
+    Medium,
+    High
+}
+
+impl Limit{
+    fn value(&self) -> usize {
+        match *self {
+            Limit::Low => 256,
+            Limit::Medium => 512,
+            Limit::High => 1024
+        }
+    }
+}
+
 fn main() {
-    
+    let args = Args::parse();
+}
 
-
+fn render(pixels: &mut [u8], bounds: (usize, usize), upper_left: Complex<f64>, lower_right: Complex<f64>){
+    assert!(pixels.len() == bounds.0 * bounds.1);
     
+}
+
+fn write_image(filename: &str, pixels: &[u8], bounds: (usize, usize)) -> Result<(), std::io::Error>{
+    Ok(())
+}
+
+/// Try to prove if `c` is in the Mandelbrot set, using `limit` iterations.
+/// 
+/// If `c` is proven to be outside of the set, return `Some(i)`,
+/// where `i` is the number of iterations it took to prove it
+/// was not a member of the set. Otherwise, if not proven to be
+/// outisde the set, return `None`.
+fn escape_time(c: Complex<f64>, limit: Limit) -> Option<usize>{
+    let mut z = Complex {re: 0.0, im: 0.0};
+    for i in 0..limit.value(){
+        if z.norm_sqr() > 4.0 {
+            return Some(i);
+        }
+        z = z * z + c;
+    }
+    None
+}
+
+fn pixel_to_point(bounds: (usize, usize), pixel: (usize, usize), upper_left: Complex<f64>, lower_right: Complex<f64>) -> Complex<f64>{
+    let (width, height) = (lower_right.re - upper_left.re,
+                                upper_left.im - lower_right.im);
+    Complex {
+        re: upper_left.re + pixel.0 as f64 * width / bounds.0 as f64,
+        im: upper_left.im - pixel.1 as f64 * width / bounds.1 as f64
+    }
+}
+
+#[test]
+fn test_pixel_to_point(){
+    assert_eq!(pixel_to_point((100, 200), 
+                                (25, 175), 
+                                Complex{re: -1.0, im: 1.0}, 
+                                Complex{re: 1.0, im: -1.0}),
+                Complex{re: -0.5, im: -0.75});
 }
